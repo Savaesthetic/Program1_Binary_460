56d00ebe3ea9aae9e6cbf2b682394d453c4af76c
Savaesthetic <asavaus@gmail.com>
2022-01-12 14:20:52 -0700

    Setup project.

---
+++ Prog1A.java
@@ -0,0 +1,25 @@
+import java.io.*;
+
+class DataRecord {
+    public static final int RECORD_LENGTH = 16;
+    private final int COUNTY_NAME_LENGTH = 22; 
+
+    private String projectId;
+    private String projectName;
+    private String status;
+    private String scope;
+    private String type;
+    private String methodology;
+    private String region;
+    private String country;
+    private String state;
+    private int creditsIssued;
+    private int creditsRetired;
+    private int creditsRemaining;
+    private int firstYear;
+}
+public class Prog1A {
+    public static void main (String [] args) {
+
+    }
+}
